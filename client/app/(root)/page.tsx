3443b981cd526b295c4bf67f148234f9c0d54aa0
Karabo Mdluli <kayak.karabo@gmail.com>
2024-06-05 05:00:56 +0200

    implement layout

---
+++ client/app/(root)/page.tsx
@@ -0,0 +1,24 @@
+
+
+const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
+  const loggedIn = { firstName: 'Karabo'}
+
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          Welcome
+        </header>
+
+          RECENT         
+      </div>
+
+      
+
+      
+    </section>
+  )
+}
+
+export default Home
