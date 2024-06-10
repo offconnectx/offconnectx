fc65722c718d1cdae9cea34dec3fba2e8dc5d7e9
sm-remi023 <sm-remi023@student.wethinkcode.co.za>
2024-06-10 22:02:25 +0200

    started landing page

--- client/app/(connect)/landing/page.tsx
+++ client/app/(connect)/landing/page.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import NavBar from '@/components/NavBar';
+import Hero from '@/components/Hero';
 
-const page = () => {
+const LandingPage = () => {
   return (
-    <div>page</div>
-  )
+    <main className=" bg-gray-900 flex flex-col items-center justify-between">
+      <div className="absolute relative w-full w-[100vw] h-[100vh]">
+        <NavBar/>
+        <Hero/>
+
+      </div>
+    </main>
+  ) 
 }
 
-export default page
+export default LandingPage;
