0f0859ca417f962d2fdc038fff3316d6a1e75b16
Karabo Mdluli <kayak.karabo@gmail.com>
2024-06-17 00:58:50 +0200

    constants

--- client/constants/index.ts
+++ client/constants/index.ts
@@ -3,33 +3,33 @@
 export const how = [
     {
         imgURL: "/icons/defi.png",
-        label: "STEP 1 - Deposit wETH",
-        subtext: "wETH serves as collateral and is securely locked in a smart contract"
+        label: "STEP 1 - ",
+        subtext: "Users"
     },
     {
         imgURL: "/icons/defi.png",
-        label: "STEP 2 - Mint UUSD",
-        subtext: "The smart contract calculates the appropriate amount of UUSD based on the value of the locked wETH collateral and the algorithmic stability mechanism."
+        label: "STEP 2 - ",
+        subtext: "The "
     },
     {
         imgURL: "/icons/defi.png",
-        label: "STEP 3 - Annual Interest (Approx. 7% APR)",
-        subtext: "Users can choose to stake their UUSD to earn interest or they can freely spend their UUSD."
+        label: "STEP 3 - ",
+        subtext: "Uh"
     },
     {
         imgURL: "/icons/defi.png",
-        label: "STEP 4 - Mint UZAR",
-        subtext: "Users can mint UZAR using their UUSD. This stablecoin is pegged to the South African ZAR rand and is designed to facilitate cost-effective and efficient remittances to Africa."
+        label: "STEP 4 - ",
+        subtext: "ho"
     }, 
     {
         imgURL: "/icons/defi.png",
-        label: "STEP 5 - Usage Freedom",
-        subtext: "UUSD and UZAR holders can further engage with DeFi opportunities. Leverage, Yield Farming and Liquidity Provision"
+        label: "STEP 5 - ",
+        subtext: "How"
     },
     {
         imgURL: "/icons/defi.png",
-        label: "STEP 6 - Redemption Process",
-        subtext: "If a user wants to get their wETH collateral back, here's how it works: You return your UUSD tokens to the smart contract. The contract then figures out how much wETH you should receive based on the current price of ETH."
+        label: "STEP 6 - ",
+        subtext: "If a ."
     },
 ];
 
