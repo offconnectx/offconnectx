5f506c2c320ca8913c6b4f7cf49c9d71d0bfade4
sm-remi023 <sm-remi023@student.wethinkcode.co.za>
2024-06-18 18:45:32 +0200

    implemented different styles from layout branch

--- client/app/(connect)/landing/page.tsx
+++ client/app/(connect)/landing/page.tsx
@@ -7,28 +7,39 @@ import FAQ from '@/components/FAQ';
 import Footer from '@/components/Footer';
 import How from '@/components/How';
 import Stats from '@/components/Stats';
+import CTA from '@/components/CTA';
+import  Style  from '@/components/Style';
+import Clients from '@/components/Team';
+import Team from "@/components/Team";
+
 
 const LandingPage = () => {
   return (
-    <main className="bg-gray-900 min-h-screen flex flex-col items-center">
+    <main className="bg-gray-900 min-h-screen flex flex-col items-center">z
       <NavBar />
       <section id="hero" className="w-full">
         <Hero />
       </section>
-      <section id="" className="mb-60">
+      <section id="" className="mb-44">
         <Stats />
-      </section>
-      <section id="about" className="w-full  mb-60">
-        <About />
       </section>
       <section id="services" className="w-full  mb-60">
         <How />
       </section>
+      <section id="about" className="w-full  mb-32s">
+        <About />
+      </section>
+      {/* <section id="" className="w-full  mb-60">
+        <Style />
+      </section> */}
       <section id="faq" className="w-full mb-24">
-        <FAQ />
+        <Team/>
       </section>
       <section id="contact" className="w-full mb-24">
         <Contact />
+      </section>
+      <section id="cta" className="w-full mb-24">
+        <CTA />
       </section>
       <section id="footer" className="w-full mb-24">
         <Footer />
---
+++ client/components/Button.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Button = ({ }) => (
+  <a href='https://'  target='new'>
+    <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-white bg-black-gradient-2 rounded-[10px] outline-none `}>
+      Enter Dapp
+    </button>
+  </a>
+);
+
+export default Button;
+
