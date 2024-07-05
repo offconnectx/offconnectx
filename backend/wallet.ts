cf6e6066a898321c73247387018114e94943ba3f
Luqxus <lebelwane@protonmail.com>
2024-07-05 15:05:49 +0200

    Update wallet.ts

--- backend/wallet.ts
+++ backend/wallet.ts
@@ -1,50 +1,24 @@
-import crypto from 'crypto';
+import { KeyPair, mnemonicToWalletKey } from "@ton/crypto";
+import { Cell, WalletContractV4 } from "@ton/ton";
 import * as bip39 from 'bip39';
-import { mnemonicToWalletKey } from '@ton/crypto';
-import { WalletContractV4 } from '@ton/ton';
+import crypto from 'crypto';
 
-
-
-
-async function  createWallet(pwd:string) {
-
-	// TODO: generate mnemonic
-	const mnemonic = generateMnemonic()
-
-	// TODO: create a password
-	const encryptedMnemonic = encryptMnemonic(mnemonic, pwd);
-
-	// TODO: hash the password;
-	const hashedPassword = hashString(pwd);
-	
-	// TODO: store encrypted mnemonic & hashed password in secure storage
-
-}
-
-
-async function loadWallet(plainPassword: string): Promise<WalletContractV4> {
-	// TODO: gets a mnemonic from storage
-	const encryptedMnemonic = getWalletMnemonic();
+// async function loadWallet(plainPassword: string): Promise<WalletContractV4> {
+// 	// TODO: gets a mnemonic from storage
+// 	const encryptedMnemonic = await getWalletMnemonic();
 	
 
-	// TODO: decrypt mnemonic & generate keypair
-	const key = await mnemonicToWalletKey(
-		decryptMnemonic(encryptedMnemonic,plainPassword).split(" ")
-	);
+// 	// TODO: decrypt mnemonic & generate keypair
+// 	const key = await mnemonicToWalletKey(
+// 		decryptMnemonic(encryptedMnemonic,plainPassword).split(" ")
+// 	);
 
-	// return wallet from keypair pubKey and workchain
-	return WalletContractV4.create({publicKey: key.publicKey, workchain: 0});
-}
-
-
-function getWalletMnemonic(): string {
-	// TODO: get encrypted mnemonic from secure local storage
-	const encryptedMnemonic = "";
-	return encryptedMnemonic;
-}
+// 	// return wallet from keypair pubKey and workchain
+// 	return WalletContractV4.create({publicKey: key.publicKey, workchain: 0});
+// }
 
 
-function generateMnemonic(): string {
+export function generateMnemonic(): string {
 	const entropy = crypto.randomBytes(32);
 
 	// Generate a BIP39 mnemonic phrase from the random bytes
@@ -53,40 +27,46 @@ function generateMnemonic(): string {
 	return mnemonic;
 }
 
-function encryptMnemonic(mnemonic: string, pwd: string): string {
+export async function getSecretKey(pwd: string, mnemonic: string): Promise<KeyPair> {
 	
-	const iv = crypto.randomBytes(16);
-
-	const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(pwd), iv);
-
-	let encrypted = cipher.update(mnemonic, 'utf-8', 'hex');
-	encrypted += cipher.final('hex');
-	return iv.toString('hex') + "." + encrypted;
+	// TODO: decrypt mnemonic & generate keypair
+	return await mnemonicToWalletKey(
+		mnemonic.split(" "),pwd
+	);
 }
 
+// async function getWalletMnemonic(): Promise<string> {
+// 	// TODO: get encrypted mnemonic from secure local storage
+// 	const encryptedMnemonic = "";
+// 	return encryptedMnemonic;
+// }
 
-function decryptMnemonic(encrypedText: string, pwd: string): string {
+export function decryptMnemonic(encrypedText: string, pwd: string): string {
+	let key = crypto.createHash('sha256').update(String(pwd)).digest();
 	const parts = encrypedText.split(".");
 	const iv = Buffer.from(parts[0], 'hex');
 
-	const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(pwd), iv);
+	const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
 	let decrypted = decipher.update(parts[1], 'hex', 'utf-8');
 	decrypted += decipher.final('utf-8');
 
 	return decrypted;
 }
 
+export function encryptMnemonic(mnemonic: string, pwd: string): string {
+	let key = crypto.createHash('sha256').update(String(pwd)).digest();
+	const iv = crypto.randomBytes(16);
 
-function hashString(plainString: string): string {
-	return crypto.hash('sha256', plainString);
+	const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+
+	let encrypted = cipher.update(mnemonic, 'utf-8', 'hex');
+	encrypted += cipher.final('hex');
+	return iv.toString('hex') + "." + encrypted;
 }
 
-function validateHashString(hashedString: string,plainString: string): boolean {
-	return hashedString == crypto.hash('sha256', plainString);
+export function serialize(cell: Cell) {
+	// TODO: save boc to ecrypted local storage
+	const boc = cell.toBoc();
+	// fs.writeFileSync('txx.boc', boc);
+	return boc
 }
-
-
-
-function importWallet(mnemonic: string) {
-	// TODO: this function will recover a wallet using as mnemonic phrase
-} 
