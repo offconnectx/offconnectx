023ac2f944185ab487f677d7f8d1651a7937929a
luqxus <lebelwane@protonmain.com>
2024-07-14 23:43:53 +0200

    get started button padding

--- client/components/Hero.tsx
+++ client/components/Hero.tsx
@@ -4,7 +4,7 @@ import Modal from "./Modal";
 
 const Hero = () => {
   return (
-    <div className="pt-32 md:pt-0 md:top-1/4 lg:top-1/3 max-w-7xl mx-auto flex justify-center text-center min-h-screen">
+    <div className="pt-32 mb-24 md:pt-0 md:top-1/4 lg:top-1/3 max-w-7xl mx-auto flex justify-center text-center min-h-screen">
       <div className="mt-16 max-w-4xl px-4 flex flex-col gap-8 md:max-xl">
         <h1 className="max-w-xl text-[-2.9rem] leading-none text-gray-200 lg:text-5xl lg:leading-tight mb-5 -ml-10 text-5xl md:text-6xl font-semibold">
           Make <span className="text-sky-400">Offline</span> Transactions.
